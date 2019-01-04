b5347aef3287b110ce47e3e6bf569cac1c11aecf
Bex Edmondson <bex.edmondson@hutchgames.com>
2019-01-04 01:08:04 +0000

    THE BOWL SPINSSSSS

--- Assets/Scripts/Bowl.cs
+++ Assets/Scripts/Bowl.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Bowl : MonoBehaviour, IPointerClickHandler
+public class Bowl : MonoBehaviour, IPointerClickHandler, IDragHandler
 {
 	[SerializeField]
-	private SpriteRenderer teaMix;
+	private Rigidbody2D teaMix;
+
+	private const float k_forceMultiplier = 0.01f;
+
+	public void OnDrag(PointerEventData eventData)
+	{
+		teaMix.AddForceAtPosition(eventData.delta * Time.deltaTime * k_forceMultiplier, eventData.position, ForceMode2D.Force);
+	}
 
 	public void OnPointerClick(PointerEventData pointerEventData)
     {      
@@ -23,17 +30,17 @@ public class Bowl : MonoBehaviour, IPointerClickHandler
 
 	private IEnumerator SpinTea()
 	{
-		int i = 0;
+		//int i = 0;
 
-		while (i < 365)
-		{
-			i += 5;
-			teaMix.transform.Rotate(new Vector3(0, 0, 10f));
-			yield return null;
-		}
+		//while (i < 365)
+		//{
+			//i += 5;
+		//teaMix.AddForceAtPosition(new Vector2(10, 10), new Vector2(5, 5), ForceMode2D.Force);    //.Rotate(new Vector3(0, 0, 10f));
+		yield return null;
+		//}
 
-		teaMix.gameObject.SetActive(false);
+		//teaMix.gameObject.SetActive(false);
 
-		UIManager.instance.OnTeaMixed();
+		//UIManager.instance.OnTeaMixed();
 	}
 }
