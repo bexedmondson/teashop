34b0f987f147119f5ed768666906370d122919b9
Bex Edmondson <bex.edmondson@hutchgames.com>
2019-01-07 21:40:03 +0000

    ugh, github why didn't you commit this stuff

--- Assets/Scripts/CustomerManager.cs
+++ Assets/Scripts/CustomerManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CustomerLibrary : MonoBehaviour {
+public class CustomerManager : MonoBehaviour {
 
 	public List<CustomerData> customerDataList;
 
