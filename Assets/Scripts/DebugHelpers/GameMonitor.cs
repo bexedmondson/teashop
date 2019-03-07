dfbabb68e3b8d5cc6732ffe2d6c9d723127fca3c
Bex Edmondson <bex.edmondson@hutchgames.com>
2019-03-07 09:38:53 +0000

    adding debug window

---
+++ Assets/Scripts/DebugHelpers/GameMonitor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public static class GameMonitor
+{
+#if UNITY_EDITOR
+
+	public static string mostRecentDayStateName;
+
+	[UnityEditor.Callbacks.DidReloadScripts]
+	private static void OnScriptsReloaded()
+	{
+		//grab all day state objects so this can start listening to them all
+		DayState[] dayStates = (DayState[])Resources.FindObjectsOfTypeAll(typeof(DayState));
+		Debug.Log("[GameMonitor] Found " + dayStates.Length + " DayState objects.");
+	}
+    
+	public static void OnDayStateChanged(DayState dayState)
+	{
+		mostRecentDayStateName = dayState.name;
+		EditorWindow.GetWindow(typeof(GameMonitorWindow)).Repaint();
+	}
+
+#endif
+}
---
+++ Assets/Scripts/DebugHelpers/GameMonitorWindow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[InitializeOnLoad]
+public class GameMonitorWindow : EditorWindow
+{
+	[MenuItem("Window/Teashop Monitor")]
+	public static void ShowWindow()
+    {
+		EditorWindow.GetWindow(typeof(GameMonitorWindow));
+    }
+
+	private void OnGUI()
+	{
+		GUILayout.Label(Application.isPlaying ? "CURRENT VALUES" : "MOST RECENT VALUES", EditorStyles.largeLabel);
+
+		GUILayout.BeginHorizontal();
+		GUILayout.Label("Day State: ", EditorStyles.boldLabel);
+		GUILayout.Label(GameMonitor.mostRecentDayStateName);
+		GUILayout.EndHorizontal();
+
+		if (Application.isPlaying)
+    		EditorUtility.SetDirty(this);
+	}
+
+	private void Awake()
+	{
+		EditorApplication.playModeStateChanged += LogPlayModeState;
+	}
+
+    private void LogPlayModeState(PlayModeStateChange state)
+    {
+		OnGUI();
+    }
+}
